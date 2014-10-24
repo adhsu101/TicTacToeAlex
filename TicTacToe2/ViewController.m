77d63f28d367d1ca19cc06b03bace9df90594998
Mobile Making <mm@Alexs-MacBook-Pro-2.local>
2014-10-24 12:24:19 -0700

    solved turn algorithm

--- TicTacToe2/ViewController.m
+++ TicTacToe2/ViewController.m
@@ -21,6 +21,7 @@
 @property (nonatomic) BOOL isPlayerO;
 @property (weak, nonatomic) IBOutlet UILabel *playerLabel;
 @property (strong, nonatomic) IBOutletCollection(UILabel) NSArray *labels;
+
 @end
 
 @implementation ViewController
@@ -30,6 +31,19 @@
     [super viewDidLoad];
     self.isPlayerO = NO;
     self.playerLabel.text = @"Turn: Player X";
+    
+    // array of labels
+    
+    // set label tags
+    int labelTag = 1;
+    for (UILabel* label in self.labels)
+    {
+        NSLog(@"Label tag is %d", labelTag);
+        label.tag = labelTag;
+        labelTag ++;
+    }
+    
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -37,21 +51,63 @@
 {
     CGPoint point = [tapLabel locationInView:self.view];
     UILabel *tappedLabel = [self findLabelUsingPoint:point];
-    if (tappedLabel != nil)
-    {
-        if (self.isPlayerO){
-            tappedLabel.text = @"O";
-            self.playerLabel.text = @"Turn: Player X";
-        }
-        else
+    
+    if ([tappedLabel.text isEqualToString:@""]) {
+        
+        if (tappedLabel != nil)
         {
-            tappedLabel.text = @"X";
-            self.playerLabel.text = @"Turn: Player O";
+            if (self.isPlayerO){
+                tappedLabel.text = @"O";
+                self.playerLabel.text = @"Turn: Player X";
+                tappedLabel.textColor = [UIColor blueColor];
+            }
+            else
+            {
+                tappedLabel.text = @"X";
+                self.playerLabel.text = @"Turn: Player O";
+                tappedLabel.textColor = [UIColor redColor];
+            }
+        self.isPlayerO = !self.isPlayerO;
         }
-    self.isPlayerO = !self.isPlayerO;
     }
+    
+    switch (tappedLabel.tag)
+    {
+        case 1:
+            NSLog (@"one");
+            break;
+        case 2:
+            NSLog (@"two");
+            break;
+        case 3:
+            NSLog (@"three");
+            break;
+        case 4:
+            NSLog (@"four");
+            break;
+        case 5:
+            NSLog (@"five");
+            break;
+        case 6:
+            NSLog (@"six");
+            break;
+        case 7:
+            NSLog (@"sev");
+            break;
+        case 8:
+            NSLog (@"eight");
+            break;
+        case 9:
+            NSLog (@"nine");
+            break;
+        default:
+            NSLog (@"Integer out of range");
+            break;
+    }
+    
 }
 
+# pragma mark - Helper
 
 -(UILabel *)findLabelUsingPoint: (CGPoint) point
 {
@@ -66,5 +122,48 @@
     return nil;
 }
 
+-(BOOL)col1Win
+{
+    if ([self.label1.text isEqualToString:self.label2.text] && [self.label1.text isEqualToString:self.label3.text]) {
+        return YES;
+    }
+    NSLog(@"Winner!");
+    return NO;
+}
+
+-(BOOL)col2Win
+{
+    return YES;
+}
+
+-(BOOL)col3Win
+{
+    return YES;
+}
+
+-(BOOL)row1Win
+{
+    return YES;
+}
+
+-(BOOL)row2Win
+{
+    return YES;
+}
+
+-(BOOL)row3Win
+{
+    return YES;
+}
+
+-(BOOL)diag1To9Win
+{
+    return YES;
+}
+
+-(BOOL)diag3To7Win
+{
+    return YES;
+}
 
 @end
