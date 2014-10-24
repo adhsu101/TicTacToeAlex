c6afecc61321d4f39845934325e371873b113f8c
Valak12 <Kohnechi@gmail.com>
2014-10-23 19:07:56 -0700

    Initial Commit

---
+++ TicTacToe2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TicTacToe2
+//
+//  Created by Vala Kohnechi on 10/23/14.
+//  Copyright (c) 2014 Vala Kohnechi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TicTacToe2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TicTacToe2
+//
+//  Created by Vala Kohnechi on 10/23/14.
+//  Copyright (c) 2014 Vala Kohnechi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
