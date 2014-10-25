f7e09738e30ea2b7c4696cc67873b50a079e61c0
Valak12 <Kohnechi@gmail.com>
2014-10-25 13:49:54 -0700

    Added Help Button

---
+++ TicTacToe2/HelpViewController.h
@@ -0,0 +1,13 @@
+//
+//  HelpViewController.h
+//  TicTacToe2
+//
+//  Created by Vala Kohnechi on 10/25/14.
+//  Copyright (c) 2014 Vala Kohnechi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelpViewController : UIViewController
+
+@end
---
+++ TicTacToe2/HelpViewController.m
@@ -0,0 +1,33 @@
+//
+//  HelpViewController.m
+//  TicTacToe2
+//
+//  Created by Vala Kohnechi on 10/25/14.
+//  Copyright (c) 2014 Vala Kohnechi. All rights reserved.
+//
+
+#import "HelpViewController.h"
+#import "ViewController.h"
+
+@interface HelpViewController ()
+<UIWebViewDelegate>
+@property (strong, nonatomic) IBOutlet UIWebView *helpWebView;
+
+@end
+
+@implementation HelpViewController
+
+-(void) loadURL :(NSString *)theURL
+{
+    NSURL *url = [NSURL URLWithString:theURL];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    [self.helpWebView loadRequest:request];
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self loadURL:@"http://en.wikipedia.org/wiki/Tic-tac-toe"];
+    
+}
+
+@end
